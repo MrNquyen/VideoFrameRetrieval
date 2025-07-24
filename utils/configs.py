40a8c4539cecc34188cde50afe3f24b85589fa4e
NguyenPhamPhuongNam <162479181+NguyenPhamPhuongNam@users.noreply.github.com>
2025-07-24 22:23:06 +0700

    configs.py

--- utils/configs.py
+++ utils/configs.py
@@ -9,10 +9,14 @@ class Config():
         self.config_beit = self.config_base["beit"]
         self.config_transform = self.config_base["transform"]
         self.config_captioner = self.config_base["captioner"]
+        self.config_yolo = self.config_base.get("yolo", {})
+        self.config_storage = self.config_base.get("storage", {})
         
     def build_registry(self):
         registry.set_module("config", name="base", instance=self.config_base)
         registry.set_module("config", name="beit", instance=self.config_beit)
         registry.set_module("config", name="captioner", instance=self.config_captioner)
         registry.set_module("config", name="transform", instance=self.config_transform)
+        registry.set_module("config", name="yolo", instance=self.config_yolo)
+        registry.set_module("config", name="storage", instance=self.config_storage)
             
